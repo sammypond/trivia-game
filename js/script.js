97548761f456d3017fe3a77c0778361f6fee9b96
Samuel Pond Marritz <kim@Samuels-MacBook-Air.local>
2019-06-09 12:46:04 -0700

    win logic added

--- js/script.js
+++ js/script.js
@@ -16,12 +16,12 @@ var input = document.getElementById('input');
 //api address = https://opentdb.com/api.php?amount=10
 // add event listeners
 
-getQuestion.addEventListener('click', function(e){
+document.addEventListener('DOMContentLoaded', function (e) {
     fetch(aPi)
-        .then(function(results){
+        .then(function (results) {
             return results.json();
         })
-        .then(function (json){
+        .then(function (json) {
             var questions = json.results;
             var question = questions[0].question;
             console.log(questions);
@@ -29,21 +29,48 @@ getQuestion.addEventListener('click', function(e){
 
             var questionBox = document.getElementById('question');
             questionBox.textContent = question;
-        })
-    submit.addEventListener('click', function(e){
-        fetch(aPi)
-            .then(function(results){
-                console.log(results);
-                return results.json();
-            })
-            .then(function(json){
+
+            submit.addEventListener('click', function (e) {
                 var answers = json.results;
+                console.log(json.results);
                 var answer = answers[0].correct_answer;
                 console.log(answers);
                 console.log(answer);
-                if(answer === input){
+                if (answer === input.value) {
                     alert("correct");
                 }
+                input.value = '';
+
             })
+        })
+    getQuestion.addEventListener('click', function (e) {
+        fetch(aPi)
+            .then(function (results) {
+                return results.json();
+            })
+            .then(function (json) {
+                var questions = json.results;
+                var question = questions[0].question;
+                console.log(questions);
+                console.log(questions[0].question);
+
+                var questionBox = document.getElementById('question');
+                questionBox.textContent = question;
+
+                submit.addEventListener('click', function (e) {
+                    var answers = json.results;
+                    console.log(json.results);
+                    var answer = answers[0].correct_answer;
+                    console.log(answers);
+                    console.log(answer);
+                    if (answer === input.value) {
+                        alert("correct");
+                    }
+                    input.value = '';
+                })
+            })
+
     })
+
 })
+
