347dffcaf5836208b187780adb8f9a986122e5b2
Samuel Pond Marritz <kim@Samuels-MacBook-Air.local>
2019-06-10 16:47:07 -0700

    firt iteration of mult-choice play

--- js/script2.js
+++ js/script2.js
@@ -13,7 +13,29 @@ var questionIndex = 0;
 var questionBox;
 var input = document.getElementById('input');
 var getQuestion = document.getElementById('questionbutton');
-var submit = document.getElementById('submit');
+var answerButtons = document.getElementById('radiocity');
+var questions;
+var question;
+var answer;
+var false1;
+var false2;
+var false3;
+var questionBox;
+var answerButtons;
+var answerLabels;
+var answerLabel1;
+var answerLabel2;
+var answerLabel3;
+var answerLabel4;
+var answer1;
+var answer2;
+var answer3;
+var answer4;
+var questionCounter = 0;
+var turnCounter = 0;
+var scorea = document.getElementById('scorea');
+var scoreb = document.getElementById('scoreb');
+var wrongMessage = document.getElementById('invisible');
 
 //call JSON query 
 
@@ -22,45 +44,108 @@ fetch(aPi)
         return results.json();
     })
     .then(function (json) {
-        var questions = json.results;
-        var question = questions[0].question;
-        var answer = questions[0].correct_answer;
-        var false1 = questions[0].incorrect_answers[0];
-        var false2 = questions[0].incorrect_answers[1];
-        var false3 = questions[0].incorrect_answers[2];
-        var questionBox = document.getElementById('question');
+        questions = json.results;
+        question = questions[0].question;
+        answer = questions[0].correct_answer;
+        false1 = questions[0].incorrect_answers[0];
+        false2 = questions[0].incorrect_answers[1];
+        false3 = questions[0].incorrect_answers[2];
+        questionBox = document.getElementById('question');
         questionBox.textContent = question;
-        console.log(questions);
-        console.log(question);
-        console.log(answer);
-        console.log(false1);
-        console.log(false2);
-        console.log(false3);
-        var answerButtons = document.getElementsByTagName('input');
-        var answerLabels = document.getElementsByTagName('label');
-        var answerLabel1 = answerLabels[0]
-        var answerLabel2 = answerLabels[1];
-        var answerLabel3 = answerLabels[2];
-        var answerLabel4 = answerLabels[3];
-        var answer1 = answerButtons[0];
-        var answer2 = answerButtons[1];
-        var answer3 = answerButtons[2];
-        var answer4 = answerButtons[3];
+        answerButtons = document.getElementsByTagName('input');
+        answerLabels = document.getElementsByTagName('label');
+        answerLabel1 = answerLabels[0]
+        answerLabel2 = answerLabels[1];
+        answerLabel3 = answerLabels[2];
+        answerLabel4 = answerLabels[3];
+        answer1 = answerButtons[0];
+        answer2 = answerButtons[1];
+        answer3 = answerButtons[2];
+        answer4 = answerButtons[3];
         answer1.value = answer;
         answer2.value = false1;
         answer3.value = false2;
-        answer4.value = false3; 
+        answer4.value = false3;
         answerLabel1.textContent = answer;
         answerLabel2.textContent = false1;
         answerLabel3.textContent = false2;
         answerLabel4.textContent = false3;
-
-        getQuestion.addEventListener('click', function(e){
-            var questionCounter = 0;
-            questionCounter++;
-            question = question[questionCounter]
-        })
-    
+        questionCounter++;
     })
 
+// getQuestion.addEventListener('click', function(e){
+//     questionCounter++;
+//     question = questions[questionCounter].question;
+//     questionBox.textContent = question;
+//     answer = questions[questionCounter].correct_answer;
+//     false1 = questions[questionCounter].incorrect_answers[0];
+//     false2 = questions[questionCounter].incorrect_answers[1];
+//     false3 = questions[questionCounter].incorrect_answers[2];
+//     questionBox = document.getElementById('question');
+//     questionBox.textContent = question;
+//     answerButtons = document.getElementsByTagName('input');
+//     answerLabels = document.getElementsByTagName('label');
+//     answerLabel1 = answerLabels[0]
+//     answerLabel2 = answerLabels[1];
+//     answerLabel3 = answerLabels[2];
+//     answerLabel4 = answerLabels[3];
+//     answer1 = answerButtons[0];
+//     answer2 = answerButtons[1];
+//     answer3 = answerButtons[2];
+//     answer4 = answerButtons[3];
+//     answer1.value = answer;
+//     answer2.value = false1;
+//     answer3.value = false2;
+//     answer4.value = false3; 
+//     answerLabel1.textContent = answer;
+//     answerLabel2.textContent = false1;
+//     answerLabel3.textContent = false2;
+//     answerLabel4.textContent = false3;
+// })
+
+answerButtons.addEventListener('click', function (e) {
+    turnCounter ++; 
+    if (e.target.value == answer && turnCounter % 2 === 0) {
+        ascore = ascore + 1;
+        scorea.textContent = ascore;
+    } else if (e.target.value == answer && turnCounter % 2 !== 0) {
+        bscore = bscore + 1;
+        scoreb.textContent = bscore;
+    } else {
+        alert('Incorrect!');
+    }
     
+
+
+    // var shuffle = document.querySelector('radiocity');
+    // for (var i = shuffle.children.length; i >= 0; i--) {
+    //     shuffle.appendChild(shuffle.children[Math.random() * i | 0]);
+    // }
+    questionCounter++;
+    question = questions[questionCounter].question;
+    questionBox.textContent = question;
+    answer = questions[questionCounter].correct_answer;
+    false1 = questions[questionCounter].incorrect_answers[0];
+    false2 = questions[questionCounter].incorrect_answers[1];
+    false3 = questions[questionCounter].incorrect_answers[2];
+    questionBox = document.getElementById('question');
+    questionBox.textContent = question;
+    answerButtons = document.getElementsByTagName('input');
+    answerLabels = document.getElementsByTagName('label');
+    answerLabel1 = answerLabels[0]
+    answerLabel2 = answerLabels[1];
+    answerLabel3 = answerLabels[2];
+    answerLabel4 = answerLabels[3];
+    answer1 = answerButtons[0];
+    answer2 = answerButtons[1];
+    answer3 = answerButtons[2];
+    answer4 = answerButtons[3];
+    answer1.value = answer;
+    answer2.value = false1;
+    answer3.value = false2;
+    answer4.value = false3;
+    answerLabel1.textContent = answer;
+    answerLabel2.textContent = false1;
+    answerLabel3.textContent = false2;
+    answerLabel4.textContent = false3;
+})
