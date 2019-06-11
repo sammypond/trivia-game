a97e6f4d471e16d744792e38fd1ec1bffd2adccd
Samuel Pond Marritz <kim@Samuels-MacBook-Air.local>
2019-06-11 09:46:03 -0700

    updating game with shuffle method

--- js/script2.js
+++ js/script2.js
@@ -36,6 +36,9 @@ var turnCounter = 0;
 var scorea = document.getElementById('scorea');
 var scoreb = document.getElementById('scoreb');
 var wrongMessage = document.getElementById('invisible');
+var correct;
+var myAnswers;
+var correct = answer
 
 //call JSON query 
 
@@ -71,56 +74,24 @@ fetch(aPi)
         answerLabel3.textContent = false2;
         answerLabel4.textContent = false3;
         questionCounter++;
+        
     })
 
-// getQuestion.addEventListener('click', function(e){
-//     questionCounter++;
-//     question = questions[questionCounter].question;
-//     questionBox.textContent = question;
-//     answer = questions[questionCounter].correct_answer;
-//     false1 = questions[questionCounter].incorrect_answers[0];
-//     false2 = questions[questionCounter].incorrect_answers[1];
-//     false3 = questions[questionCounter].incorrect_answers[2];
-//     questionBox = document.getElementById('question');
-//     questionBox.textContent = question;
-//     answerButtons = document.getElementsByTagName('input');
-//     answerLabels = document.getElementsByTagName('label');
-//     answerLabel1 = answerLabels[0]
-//     answerLabel2 = answerLabels[1];
-//     answerLabel3 = answerLabels[2];
-//     answerLabel4 = answerLabels[3];
-//     answer1 = answerButtons[0];
-//     answer2 = answerButtons[1];
-//     answer3 = answerButtons[2];
-//     answer4 = answerButtons[3];
-//     answer1.value = answer;
-//     answer2.value = false1;
-//     answer3.value = false2;
-//     answer4.value = false3; 
-//     answerLabel1.textContent = answer;
-//     answerLabel2.textContent = false1;
-//     answerLabel3.textContent = false2;
-//     answerLabel4.textContent = false3;
-// })
 
 answerButtons.addEventListener('click', function (e) {
-    turnCounter ++; 
-    if (e.target.value == answer && turnCounter % 2 === 0) {
+    turnCounter++;
+    if (e.target.value == answer && turnCounter % 2 !== 0) {
         ascore = ascore + 1;
         scorea.textContent = ascore;
-    } else if (e.target.value == answer && turnCounter % 2 !== 0) {
+    } else if (e.target.value == answer && turnCounter % 2 === 0) {
         bscore = bscore + 1;
         scoreb.textContent = bscore;
     } else {
         alert('Incorrect!');
     }
+
     
 
-
-    // var shuffle = document.querySelector('radiocity');
-    // for (var i = shuffle.children.length; i >= 0; i--) {
-    //     shuffle.appendChild(shuffle.children[Math.random() * i | 0]);
-    // }
     questionCounter++;
     question = questions[questionCounter].question;
     questionBox.textContent = question;
@@ -148,4 +119,24 @@ answerButtons.addEventListener('click', function (e) {
     answerLabel2.textContent = false1;
     answerLabel3.textContent = false2;
     answerLabel4.textContent = false3;
+    
+    shuffle();
+
+    function shuffle(myAnswers) {
+        myAnswers = [answer, false1, false2, false3];
+
+        var i,
+            j,
+            temp;
+        for (i = myAnswers.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            temp = myAnswers[i];
+            myAnswers[i] = myAnswers[j];
+            myAnswers[j] = temp;
+        }
+        console.log(myAnswers);
+        return myAnswers;    
+    };
+    
+
 })
